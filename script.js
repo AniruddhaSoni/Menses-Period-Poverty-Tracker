49c9e8dab9c15020dbb949289950bb9ecc72e1ca
bhartik021 <bhartik021@gmail.com>
2022-03-30 13:55:54 +0530

    updated

---
+++ script.js
@@ -0,0 +1,9 @@
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8,
+        mapId: '42615760941f5f34'
+        });
+}
+
+// 23.117909999332866, 77.89599075754552
