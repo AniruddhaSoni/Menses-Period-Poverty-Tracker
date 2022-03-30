27c21d571bf8927c8fd67a6d2b7503b77b1af0f6
bhartik021 <bhartik021@gmail.com>
2022-03-30 14:19:26 +0530

    updated

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ function initMap() {
         zoom: 8,
         mapId: '42615760941f5f34'
         });
+
+        new google.maps.Marker({
+            position: { lat: -25.363, lng: 131.044 },
+            map,
+            title: "Hello World!",
+          });
 }
 
 // 23.117909999332866, 77.89599075754552
