a5f9f30e25e44e20127b596a1a67a1538c229914
AniruddhaSoni <anirudh.soni007@gmail.com>
2022-03-31 16:44:47 +0530

    done

--- dist/js/data.js
+++ dist/js/data.js
@@ -1,45 +1,157 @@
 const womenNgos = {
   cid: 1001,
-  latlong: [
+  ngoDetails: [
     {
-      lat: 26.84060454757715,
-      lon: 75.753369052389,
+      name: "Chahat Women Welfare Foundation",
+      address:
+        "D-57,Ma Karni Nagar,Panchyawala, Vaishali Nagar, Jaipur, Rajasthan 302034",
+      phone: 8233500478,
+      lat: 27.11348798362535,
+      lon: 75.01606129439868,
     },
     {
-      lat: 28.5435415884726,
-      lon: 77.21702756401264,
+      name: "India Women Welfare Foundation",
+      address: "5 WH 17 NIT, Faridabad, 121001",
+      phone: 9868188194,
+      lat: 28.785545380901507,
+      lon: 77.78420191866772,
     },
     {
-      lat: 28.649527748525085,
-      lon: 77.17317095906722,
+      name: "Apna Ghar Ashram",
+      address:
+        "Polytechnic College, Campus Road, Chhatrapura, Dhanmandi, Kota, Rajasthan 324007",
+      phone: 8764396812,
+      lat: 25.300936004315087,
+      lon: 76.57508770263814,
     },
     {
-      lat: 28.619961208208114,
-      lon: 77.20475652328392,
+      name: "Share A Book India Association",
+      address: "C 167, Sector - C, Talwandi, Kota, Rajasthan 324005",
+      phone: 9116899611,
+      lat: 25.71656213199986,
+      lon: 76.19042401707227,
     },
     {
-      lat: 28.71842831308944,
-      lon: 77.17454398859199,
+      name: "BEING A WOMAN NGO",
+      address:
+        "F-69/301, Abul Fazal Enclave Part 1, Okhla Vihar, Block A, Jamia Nagar, Okhla, New Delhi, Delhi 110025",
+      phone: 9717560272,
+      lat: 28.78739797099648,
+      lon: 77.94882783794526,
     },
     {
-      lat: 28.588992434781378,
-      lon: 77.26374562341462,
+      name: "Nirmala Foundation । Top NGO Delhi । Top NGO India । Women & Youth Empowerment",
+      address:
+        "1, Ground, A-110, Shiv Vihar Colony, Karala, New Delhi, Delhi 110081",
+      phone: 7210449958,
+      lat: 29.00247094490669,
+      lon: 77.94855661889869,
     },
     {
-      lat: 28.488621981109116,
-      lon: 77.1995851199794,
+      name: "YASHVI WOMEN CHILD DEVELOPMENT FOUNDATION Regi.Act of Nct delhi Govt.of india",
+      address:
+        "WZ 405 SHOP G/F, Street Number 2, Janak Park, Pocket 408, Hari Nagar, New Delhi, Delhi 110064",
+      phone: 9809613458,
+      lat: 29.205846553630504,
+      lon: 76.9926103441706,
     },
     {
-      lat: 26.913644359850462,
-      lon: 75.70036799811379,
+      name: "Sambhali Trust",
+      address:
+        "Bungalow No. 1-A K. N. College, Road, Rai Ka Bagh, Jodhpur, Rajasthan 342001",
+      phone: 7849907439,
+      lat: 26.44495043205463,
+      lon: 73.60507800838633,
     },
     {
-      lat: 26.938098183272682,
-      lon: 75.77074903339577,
+      name: "Mother Concern",
+      address: "C-63, Basement, South Extension I, New Delhi, Delhi 110049",
+      phone: 8595919258,
+      lat: 29.068811029810554,
+      lon: 77.42804374853677,
     },
     {
-      lat: 26.586166276586155,
-      lon: 74.8594619778712,
+      name: "Vimochana",
+      address:
+        "8, Dodda Banaswadi Main Rd, Jaibharath Nagar, Sanjeevappa Layout, Maruthi Sevanagar, Bengaluru, Karnataka 560033",
+      phone: 8025492781,
+      lat: 13.182634091243782,
+      lon: 78.09279242143762,
+    },
+    {
+      name: "Mminority Women Welfare Society",
+      address:
+        "Plot No 68, Jafar Nagar, Near Kidwani High School, Jafar Nagar, Nagpur, Maharashtra 440013",
+      phone: 7122580827,
+      lat: 24.457150606616107,
+      lon: 81.47543559656464,
+    },
+    {
+      name: "Womenite",
+      address:
+        "Plot No 12, Vardhman Plaza, 225, Rd Number 44, Pitam Pura, New Delhi, Delhi 110034",
+      phone: 9717973658,
+      lat: 29.135681923327738,
+      lon: 78.03483751365793,
+    },
+    {
+      name: "Women for Development NGO",
+      address: "Shahinyan str. 6a, apt. 16, Gyumri 3118, Armenia",
+      phone: +37431233007,
+      lat: 40.99406898232663,
+      lon: 45.65846879585445,
+    },
+    {
+      name: "Kosovo Women's Network",
+      address:
+        "Kadri Gjata (formerly Feriz Blakqori) Str.2nd Floor, No. 8, Kosovo, Prishtina 10000",
+      phone: +38338245850,
+      lat: 42.828436946711676,
+      lon: 21.77333843941893,
+    },
+    {
+      name: "WECF, Stichting Women Engage for a Common Future",
+      address: "Korte Elisabethstraat 6, 3511 JG Utrecht, Netherlands",
+      phone: +31302310300,
+      lat: 52.41396394275052,
+      lon: 6.1702173872136665,
+    },
+    {
+      name: "European Institute of Womens Health",
+      address: "33 Pearse St, Dublin, D02 RR25, Ireland",
+      phone: +35312549700,
+      lat: 53.6088630156677,
+      lon: -5.343864985864098,
+    },
+    {
+      name: "Elrha",
+      address: "8 Cathedral Rd, Pontcanna, Cardiff CF11 9LJ, United Kingdom",
+      phone: +442032153288,
+      lat: 51.48241450125303,
+      lon: -3.1894120727574973,
+    },
+    {
+      name: "UNICEF Venezuela",
+      address:
+        "Piso 11, Av. Francisco de Miranda, Torre Parque Ávila, Caracas 1060, Miranda, Venezuela",
+      phone: +582122864094,
+      lat: 13.23630396787564,
+      lon: -61.73835409335534,
+    },
+    {
+      name: "Amnesty for Women Städtegruppe Hamburg e.V.",
+      address: "Louise-Schroeder-Straße 31, 22767 Hamburg, Germany",
+      phone: +4940384753,
+      lat: 53.551291824236465,
+      lon: 9.94938103289006,
+    },
+    {
+      name: "Women for Women International - UK",
+      address:
+        "Women for Women International, 7-14, Great Dover St, London SE1 4YR, United Kingdom",
+      phone: +447955373459,
+      lat: 51.603233009002935,
+      lon: 1.4866267375359805,
     },
   ],
 };
--- dist/js/map.js
+++ dist/js/map.js
@@ -1,62 +1,12 @@
 import womenNgos from "./data.js";
 
-const latLon = womenNgos.latlong;
-
-var allDistance = new Array();
-
-function calcRoute(start, end, map) {
-  var request = {
-    origin: start,
-    destination: end,
-    travelMode: google.maps.TravelMode.DRIVING,
-    unitSystem: google.maps.UnitSystem.IMPERIAL,
-  };
-
-  // defining directionServices
-  var directionsService = new google.maps.DirectionsService();
-
-  // defining directionsDisplay
-  var directionsDisplay = new google.maps.DirectionsRenderer();
-  // set the directions renderer to display the directions
-  directionsDisplay.setMap(map);
-
-  directionsService.route(request, function (result, status) {
-    if (status == google.maps.DirectionsStatus.OK) {
-      var distance = result.routes[0].legs[0].distance.value;
-      allDistance.push(distance);
-    }
-  });
-}
-function compareRoute(start, end, map, smallestDistance) {
-  var request = {
-    origin: start,
-    destination: end,
-    travelMode: google.maps.TravelMode.DRIVING,
-    unitSystem: google.maps.UnitSystem.IMPERIAL,
-  };
-
-  // defining directionServices
-  var directionsService = new google.maps.DirectionsService();
-
-  // defining directionsDisplay
-  var directionsDisplay = new google.maps.DirectionsRenderer();
-  // set the directions renderer to display the directions
-  directionsDisplay.setMap(map);
-
-  directionsService.route(request, function (result, status) {
-    if (status == google.maps.DirectionsStatus.OK) {
-      if (result.routes[0].legs[0].distance.value <= smallestDistance) {
-        //show route on map
-        directionsDisplay.setDirection(result);
-      }
-    }
-  });
-}
+const latLon = womenNgos.ngoDetails;
 
 navigator.geolocation.getCurrentPosition(function (position) {
   var lat = position.coords.latitude;
   var lng = position.coords.longitude;
-  var map = new google.maps.Map(document.getElementById("map"), {
+
+  const map = new google.maps.Map(document.getElementById("map"), {
     center: {
       lat: lat,
       lng: lng,
@@ -73,42 +23,15 @@ navigator.geolocation.getCurrentPosition(function (position) {
     title: "Your Location",
   });
 
-  console.log(womenNgos);
-
-  (async function iif() {
-    for (var i = 0; i < latLon.length; i++) {
-      var lat = latLon[i].lat;
-      var lon = latLon[i].lon;
-      var marker = new google.maps.Marker({
-        position: {
-          lat: lat,
-          lng: lon,
-        },
-        map: map,
-        title:
-          "< h3 >< /h3>< p >" +
-          womenNgos.latlong[i].lat +
-          "< /p>< p >" +
-          womenNgos.latlong[i].lon +
-          "< /p>",
-      });
-
-      calcRoute(mylocation.position, marker.position, map);
-    }
-  })();
-
-  setTimeout(() => {
-    var smallestDistance = Math.min(...allDistance);
-
-    for (var i = 0; i < latLon.length; i++) {
-      var lat = latLon[i].lat;
-      var lon = latLon[i].lon;
-      compareRoute(
-        mylocation.position,
-        { lat: lat, lng: lon },
-        map,
-        smallestDistance
-      );
-    }
-  }, 2000);
+  for (var i = 0; i < latLon.length; i++) {
+    var lat = latLon[i].lat;
+    var lon = latLon[i].lon;
+    var marker = new google.maps.Marker({
+      position: {
+        lat: lat,
+        lng: lon,
+      },
+      map: map,
+    });
+  }
 });
